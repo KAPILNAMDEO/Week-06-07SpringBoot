cd28f90b87678adc7653a15a54d0e4a82267387a
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-21 13:51:13 +0530

    [Kapil Namdeo]Add. Rest Controller to various method and respond hello to user.

---
+++ HelloMessagingApp/HelloMessagingApp/src/main/java/com/HelloMessagingApp/HelloController.java
@@ -0,0 +1,35 @@
+package com.HelloMessagingApp;
+
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // GET Method - Default response
+    @GetMapping
+    public String getHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    // POST Method
+    @PostMapping
+    public String postHello() {
+        return "Hello from BridgeLabz - POST Request";
+    }
+
+    // PUT Method
+    @PutMapping
+    public String putHello() {
+        return "Hello from BridgeLabz - PUT Request";
+    }
+
+    // DELETE Method
+    @DeleteMapping
+    public String deleteHello() {
+        return "Hello from BridgeLabz - DELETE Request";
+    }
+}
+
