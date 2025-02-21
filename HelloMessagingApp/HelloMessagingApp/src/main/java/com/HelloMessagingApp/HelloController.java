12c5f2a93147ebb0cf65214eb394a6ad99183955
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-21 15:10:46 +0530

    [Kapil Namdeo]Add.Method and pass name as query parameter

--- HelloMessagingApp/HelloMessagingApp/src/main/java/com/HelloMessagingApp/HelloController.java
+++ HelloMessagingApp/HelloMessagingApp/src/main/java/com/HelloMessagingApp/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 public class HelloController {
 
+    //UC1
     // GET Method - Default response
     @GetMapping
     public String getHello() {
@@ -32,9 +33,19 @@ public class HelloController {
         return "Hello from BridgeLabz - DELETE Request";
     }
 
+
+    //uc2
+    @GetMapping("/hello/query")
+    public String UC2(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    //uc3
     @GetMapping("/param/{name}")
     public String getHello(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
+
 }
 
